ae75908c18c117bad6e528feb4364cad84876683
vadegharv21 <vadegharv21@208a68db-832a-47dd-be74-bf29094a82ae>
2018-09-12 19:35:26 +0000

    User save completed

--- src/main/java/com/mytest/billapp/config/UserPermissionCheckFilter.java
+++ src/main/java/com/mytest/billapp/config/UserPermissionCheckFilter.java
@@ -33,8 +33,8 @@ public class UserPermissionCheckFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
-		HttpServletRequest req = (HttpServletRequest) request;
-		System.out.println("Hi..................."+req.getRequestURI());
+		/*HttpServletRequest req = (HttpServletRequest) request;
+		System.out.println("Hi..................."+req.getRequestURI());*/
 		chain.doFilter(request, response);
 	}
 	
